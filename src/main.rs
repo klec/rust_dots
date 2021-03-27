27a31bccd99be807e8746466c691efb5871a6ac0
Yuri Kletsun <ykletsun@lyonscg.com>
2021-03-27 04:49:12 +0200

    1000 live points

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,15 @@ struct Point {
 
 pub struct App {
     gl: GlGraphics,
-    points: [Point; 2], 
+    points: Vec<Point>, 
+}
+
+impl Point {
+    fn new() -> Self {
+        Point { position: (random().gen_range(300..500) as f64, random().gen_range(300..500) as f64),
+            speed: (random().gen::<f64>()*20.0-10.0, random().gen::<f64>()*20.0-10.0),
+            color: [1.0, 0.0, 0.0, 1.0] }
+    }
 }
 
 impl App {
@@ -30,27 +38,30 @@ impl App {
         const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
         const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
     
-        let square = rectangle::square(0.0, 0.0, 5.0);
+        let square = rectangle::square(0.0, 0.0, 2.0);
         
-        let pos : (f64, f64) = self.points[0].position;
+        let points = &self.points;
         self.gl.draw(args.viewport(), |c, gl| {
             clear(BLACK, gl);
-            let transform = c.transform.trans(pos.0, pos.1);
-            circle_arc(RED, 3.0, 0.0, 10.0, square, transform, gl)
+            for point in points.iter() {
+                let pos : (f64, f64) = point.position;
+                let transform = c.transform.trans(pos.0, pos.1);
+                circle_arc(RED, 1.0, 0.0, 1.0, square, transform, gl);
+            }
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) { //@todo process array of points
         for point in self.points.iter_mut() {
             let x = point.position.0 as u32;
-            match 200.cmp(&x) {
+            match 400.cmp(&x) {
                 Ordering::Less => point.speed.0 -= 0.2,
                 Ordering::Greater => point.speed.0 += 0.2,
                 Ordering::Equal => {},
             }
             
             let y = point.position.1 as u32;
-            match 200.cmp(&y) {
+            match 400.cmp(&y) {
                 Ordering::Less => point.speed.1 -= 0.2,
                 Ordering::Greater => point.speed.1 += 0.2,
                 Ordering::Equal => {},
@@ -65,26 +76,20 @@ impl App {
 fn main() {
     let opengl = OpenGL::V3_2;
 
-    let mut window: Window = WindowSettings::new("Floating points", [400, 400])
+    let mut window: Window = WindowSettings::new("Floating points", [800, 800])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
         .unwrap();
-    
-        let point1:Point = Point {
-            position: (random().gen_range(1..100) as f64, random().gen_range(1..100) as f64),
-            speed: (random().gen_range(-1..1) as f64, random().gen_range(-1..1) as f64),
-            color: [1.0, 0.0, 0.0, 1.0]
-        };
-        let point2:Point = Point {
-            position: (random().gen_range(1..100) as f64, random().gen_range(1..100) as f64),
-            speed: (random().gen_range(-1..1) as f64, random().gen_range(-1..1) as f64),
-            color: [1.0, 0.0, 0.0, 1.0]
-        };
+
+    let mut points: Vec<Point> = Vec::with_capacity(1000);
+    for _ in 0..points.capacity() {
+        points.push(Point::new());
+    }
         
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        points: [point1, point2],
+        points: points,
     };
 
     let mut events = Events::new(EventSettings::new());
